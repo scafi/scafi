10a6a9ad2f3ca21b0a6cb741fd1f18125d026566
roby <roberto.casadei90@gmail.com>
2017-03-13 14:20:06 +0100

    Refactoring equivalence tests into different test methods

--- core/src/test/scala/it/unibo/scafi/test/functional/TestEquivalence.scala
+++ core/src/test/scala/it/unibo/scafi/test/functional/TestEquivalence.scala
@@ -23,22 +23,28 @@ class TestEquivalence extends FunSpec with Matchers {
 
   def fullyConnectedTopologyMap(elems: Iterable[ID]): Map[ID,List[ID]] = elems.map(elem => elem -> elems.toList).toMap
 
-  checkThat("the following programs are equivalent") {
+  class Fixture {
     val random = new Random(0)
     val execSequence = Stream.continually(Random.nextInt(3)).take(100)
     val devicesAndNbrs = fullyConnectedTopologyMap(List(0,1,2))
+  }
 
-    // nbr.nbr : to be ignored
-    assertEquivalence(devicesAndNbrs, execSequence){
+  checkThat("nbr.nbr is to be ignored") {
+    val fixture = new Fixture
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence){
       foldhood(0)(_+_){
         nbr{mid()+nbr{mid()}}
       }
     }{
       2 * foldhood(0)(_+_){nbr{mid()}}
     }
+  }
 
-    // rep.nbr : to be ignored
-    assertEquivalence(devicesAndNbrs, execSequence){
+  checkThat("rep.nbr is to be ignored") {
+    val fixture = new Fixture
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence){
       foldhood(0)(_+_){
         rep (nbr{mid()}) { (old) =>
           old + nbr{old} + nbr{mid()}
@@ -50,18 +56,26 @@ class TestEquivalence extends FunSpec with Matchers {
           old * 2 + mid()
         }
     }
+  }
 
-    // fold.init nbr : to be ignored
-    assertEquivalence(devicesAndNbrs, execSequence){
+
+  checkThat("fold.init nbr is to be ignored") {
+    val fixture = new Fixture
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence){
       foldhood(0)(_+_){
         foldhood(nbr{mid()})(_+_){1}
       }
     }{
       foldhood(0)(_+_){1} * foldhood(mid())(_+_){1}
     }
+  }
 
-    // fold.fold : basic
-    assertEquivalence(devicesAndNbrs, execSequence){
+
+  checkThat("fold.fold basically works") {
+    val fixture = new Fixture
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence){
       foldhood(0)(_+_){
         foldhood(0)(_+_){1}
       }
