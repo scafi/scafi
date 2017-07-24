b8d45e8e26bcd524ff1905883c51f7b1ba608750
Roberto Casadei <roberto.casadei90@gmail.com>
2017-07-24 13:49:54 +0100

    Comments for standard sensors

--- core/src/main/scala/it/unibo/scafi/incarnations/Incarnation.scala
+++ core/src/main/scala/it/unibo/scafi/incarnations/Incarnation.scala
@@ -33,12 +33,56 @@ trait Incarnation extends Core
   }
 
   trait StandardSensors { self: AggregateProgram =>
+    /**
+      * Time forward view: expected time from the device computation to neighbor's next computation
+      * incorporating that information.
+      * For device's neighbors, it is the best estimate that the underlying system can provide.
+      * For the current device, it is like deltaTime().
+      *
+      * This is the idea, pictorially:
+      *
+      *                         PAST                    PRESENT   FUTURE
+      * TIME ----------------------------------------------|-------------->
+      *              X           N                         X
+      *                          ------------lag------------
+      *              ================deltatime==============
+      *                          ================deltatime==============
+      *              $$$$delay$$$$                          $$$delay$$$$
+      */
     def nbrDelay(): FiniteDuration = nbrvar[FiniteDuration](NBR_DELAY)
+
+    /**
+      * Time backward view: how long ago information from neighbors was received.
+      * For device's neighbors, it is the time of the computation minus the timestamp on the packet.
+      * Dropped packets temporarily increase nbrLag.
+      * For the current device, it is like deltaTime().
+      */
     def nbrLag(): FiniteDuration = nbrvar[FiniteDuration](NBR_LAG)
+
+    /**
+      * Get the distance between the current device and its neighbors.
+      */
     def nbrRange(): D = nbrvar[D](NBR_RANGE_NAME)
+
+    /**
+      * Get the direction vectors towards neighbours.
+      * @return a point of type P, assuming the currently executing device is the origin
+      */
     def nbrVector(): P = nbrvar[P](NBR_VECTOR)
+
+    /**
+      * @return the current position in space
+      */
     def currentPosition(): P = sense[P](LSNS_POSITION)
+
+    /**
+      * @return the current local time
+      */
     def currentTime(): Time = sense[Time](LSNS_TIME)
+
+    /**
+      * @return the duration since the last round of execution
+      */
     def deltaTime(): FiniteDuration = sense[FiniteDuration](LSNS_DELTA_TIME)
   }
 
