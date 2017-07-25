5f8c82dda74dec9ec470b44ea9722856ab687198
Roberto Casadei <roberto.casadei90@gmail.com>
2017-07-25 20:07:31 +0100

    Consistent header for files in 'simulator-gui' module

--- simulator-gui/src/main/scala/it/unibo/scafi/simulation/gui/view/MenuBarNorth.scala
+++ simulator-gui/src/main/scala/it/unibo/scafi/simulation/gui/view/MenuBarNorth.scala
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016-2017, Roberto Casadei, Mirko Viroli, and contributors.
+ * See the LICENCE.txt file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package it.unibo.scafi.simulation.gui.view
 
 import java.awt.{Color, GridBagConstraints, GridBagLayout, Insets}
@@ -11,8 +29,6 @@ import java.text.NumberFormat
 
 /**
   * This class represent the Application menu
-  * Created by Varini on 19/10/16.
-  * Converted/refactored to Scala by Casadei on 04/02/17
   */
 class MenuBarNorth() extends JMenuBar {
   private var menus = Vector[JMenu]()
--- simulator-gui/src/main/scala/it/unibo/scafi/simulation/gui/view/NeighborsPanel.scala
+++ simulator-gui/src/main/scala/it/unibo/scafi/simulation/gui/view/NeighborsPanel.scala
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016-2017, Roberto Casadei, Mirko Viroli, and contributors.
+ * See the LICENCE.txt file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package it.unibo.scafi.simulation.gui.view
 
 import it.unibo.scafi.simulation.gui.controller.Controller
@@ -7,10 +25,7 @@ import javax.swing._
 import java.awt._
 
 /**
-  * This is the panel where are represents
-  * the connection of neighbors.
-  * Created by Varini.
-  * Converted/refactored to Scala by Casadei on 3/02/17
+  * This is the panel where are represents the connection of neighbors.
   */
 class NeighborsPanel private[view]() extends JPanel {
   this.setSize(Toolkit.getDefaultToolkit.getScreenSize)
