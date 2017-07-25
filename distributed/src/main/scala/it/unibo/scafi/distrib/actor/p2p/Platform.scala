ad134ee6bf97f690c24138f04419fcf1352bc18b
Roberto Casadei <roberto.casadei90@gmail.com>
2017-07-25 19:56:30 +0100

    Consistent header for files in 'distributed' module

--- distributed/src/main/scala/it/unibo/scafi/distrib/actor/p2p/Platform.scala
+++ distributed/src/main/scala/it/unibo/scafi/distrib/actor/p2p/Platform.scala
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016-2017, Roberto Casadei, Mirko Viroli, and contributors.
+ * See the LICENCE.txt file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package it.unibo.scafi.distrib.actor.p2p
 
 import it.unibo.scafi.distrib.actor.{Platform => BasePlatform}
@@ -5,7 +23,6 @@ import it.unibo.scafi.platform.Platform.PlatformDependency
 import scala.concurrent.duration.DurationInt
 
 /**
- * @author Roberto Casadei
  * Specializes an [[it.unibo.scafi.distrib.actor.Platform]] into a "decentralized platform" where
  *   - Devices interacts directly with one another
  * Issues to be solved / Decisions to be taken:
