577e5495ce3d7290e3ff079011382e28d51478a5
Roberto Casadei <roberto.casadei90@gmail.com>
2018-07-30 15:39:45 +0200

    Assembly for 'core' project only for one scala version

--- travis_build.sh
+++ travis_build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -e
 sbt ++$TRAVIS_SCALA_VERSION test unidoc # publish
+
+if ["$TRAVIS_SCALA_VERSION" == "$RELEASE_SCALA_VERSION"]
+then
+    sbt ++$RELEASE_SCALA_VERSION 'project core' assembly
+fi
