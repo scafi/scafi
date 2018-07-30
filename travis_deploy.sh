e69a76a74695c8149c14d4b6dae76b3a4ed21b2c
Roberto Casadei <roberto.casadei90@gmail.com>
2018-07-30 18:58:58 +0200

    Adjustments to Travis pipeline

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,7 +2,5 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  openssl aes-256-cbc -K $encrypted_e508d9beb0ba_key -iv $encrypted_e508d9beb0ba_iv -in .travis/secrets.tar.enc -out .travis/local.secrets.tar -d;
-  tar xv -C .travis -f .travis/secrets.tar;
   sbt ++$TRAVIS_SCALA_VERSION releaseEarly;
 fi
