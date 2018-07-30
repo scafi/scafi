6069a6c26688585ebe1bcb0710203fe6460afbd4
Roberto Casadei <roberto.casadei90@gmail.com>
2018-07-30 16:13:27 +0200

    Build update with assembly for both versions

--- travis_build.sh
+++ travis_build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-sbt ++$TRAVIS_SCALA_VERSION test unidoc # publish
-
-if ["$TRAVIS_SCALA_VERSION" == "$RELEASE_SCALA_VERSION"]
-then
-    sbt ++$RELEASE_SCALA_VERSION 'project core' assembly
-fi
+sbt ++$TRAVIS_SCALA_VERSION test unidoc
+sbt ++$TRAVIS_SCALA_VERSION 'project core' assembly
