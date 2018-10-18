79615c6b2e3c3d10425eb91683f522835ca3d6f2
Roberto Casadei <robyonrails@gmail.com>
2018-10-18 13:01:01 +0200

    Process DSL: wip

--- stdlib/src/main/scala/it/unibo/scafi/lib/NewProcesses.scala
+++ stdlib/src/main/scala/it/unibo/scafi/lib/NewProcesses.scala
@@ -228,4 +228,71 @@ trait Stdlib_NewProcesses {
     private def none[T]: Option[T] = None
   }
 
+  /**
+    * Example:
+    * spawn
+    *   .where(mid == 2)
+    *   .every(30, dt())
+    *   .generateKeys[Long](k => List(k))
+    *   .run[Int,Int]{
+    *     rep(_)(_+1)
+    *   }.withArgs(1000)
+    */
+  trait ProcessDSL {
+    self: AggregateProgram with FieldUtils with CustomSpawn with TimeUtils with StateManagement with StandardSensors =>
+
+    def replicated[T,R](proc: T => R)(argument: T, period: Double, numReplicates: Int) = {
+      val lastPid = sharedTimerWithDecay(period, deltaTime().length)
+      val newProcs = if(captureChange(lastPid)) Set(lastPid) else Set[Long]()
+      sspawn[Long,T,R]((pid: Long) => (arg) => {
+        (proc(arg), if(lastPid - pid < numReplicates){ Spawn.Output } else { Spawn.External })
+      }, newProcs, argument)
+    }
+
+    trait GenerationInSpace {
+      def where(pred: Boolean): GenerationInSpaceContinuation
+
+      def inNode(id: ID): GenerationInSpaceContinuation = where(mid==id)
+      def inNodes(ids: ID*): GenerationInSpaceContinuation = where(ids.contains(mid))
+    }
+
+    trait GenerationInTime {
+      def when(pred: Boolean): GenerationInTimeContinuation
+
+      def after(delay: Double, dt: Double): GenerationInTimeContinuation = when(T(delay, dt)==0)
+      def every(period: Double, dt: Double): GenerationInTimeContinuation = when(cyclicTimerWithDecay(period, dt))
+
+      def once: GenerationInTimeContinuation = after(0, 0)
+    }
+
+    trait KeyGenerator {
+      def generateKeys[K](k: Long => List[K]): KeyGeneratorContinuation[K]
+    }
+
+    class GenerationInSpaceContinuation(val inSpace: Boolean) extends GenerationInTime {
+      override def when(pred: Boolean) = new GenerationInTimeContinuation(inSpace & pred)
+    }
+
+    class GenerationInTimeContinuation(val inSpaceTime: Boolean) extends KeyGenerator {
+      override def generateKeys[K](kgen: Long => List[K]): KeyGeneratorContinuation[K] =
+        new KeyGeneratorContinuation[K](inSpaceTime, kgen(rep(0L){ k => if(inSpaceTime) k+1 else k }))
+    }
+
+    trait ProcGenerator[K] {
+      def run[A,R](proc: A => R): ProcContinuation[K,A,R]
+    }
+
+    class KeyGeneratorContinuation[K](inSpaceTime: Boolean, keys: List[K]) extends ProcGenerator[K] {
+      override def run[A, R](proc: (A) => R): ProcContinuation[K, A, R] =
+        new ProcContinuation[K,A,R](keys.toSet, proc)
+    }
+
+    object spawn extends GenerationInSpace {
+      override def where(pred: Boolean) = new GenerationInSpaceContinuation(pred)
+    }
+
+    class ProcContinuation[K,A,R](keys: Set[K], proc: A => R) {
+      def withArgs(args: A) = spawn[K,A,R](k => a => (proc(a),true), keys, args)
+    }
+  }
 }
