aef3ae76da8b8cfae5b19534431e95ed22fcd39b
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-11-12 21:07:14 +0100

    added demo of actor simulation

---
+++ demos-new/src/main/scala/sims/actor/BasicDemo.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016-2017, Roberto Casadei, Mirko Viroli, and contributors.
+ * See the LICENCE.txt file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package sims.actor
+
+import it.unibo.scafi.incarnations.BasicSimulationIncarnation.AggregateProgram
+import it.unibo.scafi.simulation.gui.incarnation.scafi.bridge.SimulationInfo
+import it.unibo.scafi.simulation.gui.incarnation.scafi.bridge.reflection.Demo
+import it.unibo.scafi.simulation.gui.incarnation.scafi.configuration.ScafiProgramBuilder
+import it.unibo.scafi.simulation.gui.incarnation.scafi.world.ScafiWorldInitializer.Grid
+import it.unibo.scafi.simulation.gui.view.scalaFX.drawer.GradientFXOutput
+import it.unibo.scafi.simulation.gui.incarnation.scafi.bridge.actor.ActorPlatformInitializer.RadiusSimulation
+
+object BasicDemo extends App {
+  ScafiProgramBuilder (
+    Grid(4,50,50),
+    SimulationInfo(program = classOf[BasicProgram]),
+    RadiusSimulation(radius = 4),
+    neighbourRender = false,
+    outputPolicy = GradientFXOutput
+  ).launch()
+}
+@Demo
+class BasicProgram extends AggregateProgram {
+  override def main() = rep(0)(_ + 1) // the aggregate program to run
+}
+
