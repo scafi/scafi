81866f20f79e333e6bb39220ee516f0d9e717038
Roberto Casadei <robyonrails@gmail.com>
2020-04-05 20:34:33 +0200

    Unidoc only for a Scala version

--- travis_build.sh
+++ travis_build.sh
@@ -2,5 +2,8 @@
 set -e
 #sbt ++$TRAVIS_SCALA_VERSION -v test unidoc
 #sbt ++$TRAVIS_SCALA_VERSION -v 'project core' assembly
-sbt +test +unidoc
+sbt +test
+if [ "$TRAVIS_SCALA_VERSION" == "$DOC_SCALA_VERSION" ]; then
+  sbt ++$TRAVIS_SCALA_VERSION unidoc
+fi
 sbt 'project core' +assembly
