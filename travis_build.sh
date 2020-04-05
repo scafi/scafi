0ee64423961c68529aba4265c7ab481f15b3be98
Roberto Casadei <robyonrails@gmail.com>
2020-04-05 16:45:21 +0200

    Adjust sbt batch command

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 sbt ++$TRAVIS_SCALA_VERSION test unidoc
-sbt ++$TRAVIS_SCALA_VERSION 'project core' assembly
-if [ $? -ne 0 ]; then
-  sbt last
-fi
+sbt ";++$TRAVIS_SCALA_VERSION; project core; assembly"
