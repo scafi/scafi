c1350e30a456614ed9f69ac3d369e51b581e0be4
Roberto Casadei <robyonrails@gmail.com>
2020-04-25 09:48:23 +0200

    C-blocks: refactoring, additions, comments

--- core/shared/src/main/scala/it/unibo/scafi/lib/BlockC.scala
+++ core/shared/src/main/scala/it/unibo/scafi/lib/BlockC.scala
@@ -17,16 +17,16 @@ trait StdLib_BlockC {
   trait BlockC {
     self: FieldCalculusSyntax with StandardSensors =>
 
-    def smaller[V: Bounded](a: V, b: V): Boolean =
-      implicitly[Bounded[V]].compare(a, b) < 0
-
-    def findParent[V: Bounded](potential: V): ID =
-      mux(smaller(minHood { nbr(potential) }, potential)) {
-        minHood { nbr { (potential, mid()) } }._2
-      } {
-        implicitly[Bounded[ID]].top
-      }
-
+    /**
+      * Collects values down a potential field.
+      * @param potential the field providing the direction of the collection process
+      * @param acc the function describing how values are to be collected
+      * @param local the value field providing the value to be collected for each device
+      * @param Null zero value for accumulation and for neighbours for which the current device is *not* a parent in the field
+      * @tparam P type of the potential field
+      * @tparam V type of the values to be collected/accumulated
+      * @return the accumulation value for each device according to its position in the potential field
+      */
     def C[P: Bounded, V](potential: P, acc: (V, V) => V, local: V, Null: V): V =
       rep(local) { v =>
         acc(local, foldhood(Null)(acc) {
@@ -34,6 +34,45 @@ trait StdLib_BlockC {
         })
       }
 
+    /**
+      * @return the ID of the "parent device" along a `potential` field, i.e.,
+      * the neighbour with the smallest value of the potential field.
+      */
+    def findParent[V: Bounded](potential: V): ID = {
+      val (minPotential,devIdWithMinPotential) = minHood { nbr{ (potential, mid) } }
+      mux(smaller(minPotential, potential)) {
+        devIdWithMinPotential
+      } {
+        implicitly[Bounded[ID]].top
+      }
+    }
+
+    /**
+      * @return the parent device UID of the current device along field `potential`,
+      *         or None if no such a parent is there (so that the device is parent of itself).
+      */
+    def findParentOpt[V: Bounded](potential: V): Option[ID] = {
+      val minPotential = minHood { nbr{ (potential, mid) } }
+      Some(minPotential).filter(p => smaller(p._1, potential)).map(_._2)
+    }
+
+    /**
+      * @return the collection of the number of devices for which `predicate` is true
+      */
+    def collectCount(predicate: Boolean, alongPotentialField: Double): Long =
+      C[Double,Long](alongPotentialField, _+_, if(predicate) 1 else 0, 0)
+
+    /**
+      * @return the collect field of the mean of the `value` field
+      */
+    def collectMean(value: Double, alongPotentialField: Double): Double = {
+      val numNodesInArea = collectCount(true, alongPotentialField)
+      val collectedValue = C[Double,Double](alongPotentialField, _+_, value, 0)
+      collectedValue/numNodesInArea
+    }
+
+    private def smaller[V: Bounded](a: V, b: V): Boolean =
+      implicitly[Bounded[V]].compare(a, b) < 0
   }
 
 }
