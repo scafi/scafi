64f59461697e88e6b48fcb07cfbe2958937d56c5
Francesco Montelli <francesco.montelli@studio.unibo.it>
2020-08-06 17:34:39 +0200

    add more tests, just to be sure

--- tests/src/test/scala/it/unibo/scafi/test/functional/TestScafiTestUtils.scala
+++ tests/src/test/scala/it/unibo/scafi/test/functional/TestScafiTestUtils.scala
@@ -47,4 +47,43 @@ class TestScafiTestUtils extends FlatSpec {
   }
 
 
+  ManhattanNet should "Support south east detached" in {
+    val net: Network with SimulatorOps = manhattanNet(
+      side = 3,
+      northWestDetached = false,
+      northEastDetached = false,
+      southEastDetached = true,
+      southWestDetached = false)
+
+    assert(net.neighbourhood(0) == Set(1,3,4))
+    assert(net.neighbourhood(1) == Set(0,2,3,4,5))
+    assert(net.neighbourhood(2) == Set(1,4,5))
+    assert(net.neighbourhood(3) == Set(0,1,4,6,7))
+    assert(net.neighbourhood(4) == Set(0,1,2,3,5,6,7))
+    assert(net.neighbourhood(5) == Set(1,2,4,7))
+    assert(net.neighbourhood(6) == Set(3,4,7))
+    assert(net.neighbourhood(7) == Set(3,4,5,6))
+    assert(net.neighbourhood(8).isEmpty)
+  }
+
+  ManhattanNet should "Support south west detached" in {
+    val net: Network with SimulatorOps = manhattanNet(
+      side = 3,
+      northWestDetached = false,
+      northEastDetached = false,
+      southEastDetached = false,
+      southWestDetached = true)
+
+    assert(net.neighbourhood(0) == Set(1,3,4))
+    assert(net.neighbourhood(1) == Set(0,2,3,4,5))
+    assert(net.neighbourhood(2) == Set(1,4,5))
+    assert(net.neighbourhood(3) == Set(0,1,4,7))
+    assert(net.neighbourhood(4) == Set(0,1,2,3,5,7,8))
+    assert(net.neighbourhood(5) == Set(1,2,4,7,8))
+    assert(net.neighbourhood(6).isEmpty)
+    assert(net.neighbourhood(7) == Set(3,4,5,8))
+    assert(net.neighbourhood(8) == Set(4,5,7))
+  }
+
+
 }
