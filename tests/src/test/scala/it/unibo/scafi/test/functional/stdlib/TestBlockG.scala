6ac2c2182664cc5453b09e8bb9f01558ed4f2d00
Francesco Montelli <francesco.montelli@studio.unibo.it>
2020-08-10 17:36:15 +0200

    test on BlockG, initial setup

---
+++ tests/src/test/scala/it/unibo/scafi/test/functional/stdlib/TestBlockG.scala
@@ -0,0 +1,40 @@
+package it.unibo.scafi.test.functional.stdlib
+
+import it.unibo.scafi.test.FunctionalTestIncarnation._
+import it.unibo.scafi.test.functional.{ScafiAssertions, ScafiTestUtils}
+import org.scalatest._
+
+
+class TestBlockG extends FunSpec with BeforeAndAfterEach {
+  import ScafiAssertions._
+  import ScafiTestUtils._
+
+  var net: Network with SimulatorOps = manhattanNet(detachedNodesCords = Set((2,2)))
+  val infinity: Double = Double.PositiveInfinity
+
+  def restartNetwork(): Unit = {
+    net = manhattanNet(detachedNodesCords = Set((2,2)))
+    net.addSensor(name = "source", value = false)
+  }
+  override protected def beforeEach(): Unit = restartNetwork()
+
+  private[this] trait TestProgram extends AggregateProgram with StandardSensors with BlockG
+
+  describe("BlockG") {
+    describe("Should support distanceTo") {
+      it("Should work if no source is specified") {
+        exec(new TestProgram {
+          override def main(): Any = distanceTo(sense[Boolean]("source"), () => 1)
+        }, ntimes = manyRounds)(net)
+
+        assertNetworkValues((0 to 8).zip(List(
+          infinity, infinity, infinity,
+          infinity, infinity, infinity,
+          infinity, infinity, infinity
+        )).toMap)(net)
+      }
+    }
+
+  }
+
+}
