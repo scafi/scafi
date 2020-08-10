b261bb11373ea87adb481f0e05bf03fae7042690
Francesco Montelli <francesco.montelli@studio.unibo.it>
2020-08-10 18:12:52 +0200

    testing broadcast

--- tests/src/test/scala/it/unibo/scafi/test/functional/stdlib/TestBlockG.scala
+++ tests/src/test/scala/it/unibo/scafi/test/functional/stdlib/TestBlockG.scala
@@ -74,6 +74,53 @@ class TestBlockG extends FunSpec with BeforeAndAfterEach {
         )).toMap)(net)
       }
     }
+    describe("Should support distanceBetween - hop") {
+      it("The source should broadcast its value") {
+        net.chgSensorValue("source", Set(4), true)
+        exec(new TestProgram {
+          override def main(): Any = broadcast(sense[Boolean]("source"), mid(), () => 1)
+        }, ntimes = manyRounds)(net)
+
+        assertNetworkValues((0 to 8).zip(List(
+          4, 4, 4,
+          4, 4, 4,
+          4, 4, 8
+        )).toMap)(net)
+      }
+      it("Should react to a source change") {
+        net.chgSensorValue("source", Set(0), true)
+        exec(new TestProgram {
+          override def main(): Any = (broadcast(sense[Boolean]("source"), mid(), () => 1), hopGradient((sense[Boolean]("source"))))
+        }, ntimes = manyManyRounds)(net)
+
+        net.chgSensorValue("source", Set(0), false)
+        net.chgSensorValue("source", Set(7), true)
+        exec(new TestProgram {
+          override def main(): Any = broadcast(sense[Boolean]("source"), mid(), () => 1)
+        }, ntimes = manyManyRounds)(net)
+
+        assertNetworkValues((0 to 8).zip(List(
+          7, 7, 7,
+          7, 7, 7,
+          7, 7, 8
+        )).toMap)(net)
+      }
+      it("Should work with multiple sources") {
+        net.chgSensorValue("source", Set(0, 7), true)
+        exec(new TestProgram {
+          override def main(): ID = broadcast(sense[Boolean]("source"), mid(), () => 1)
+        }, ntimes = manyManyRounds)(net)
+
+        //2 and 4 are equidistant from the sources -> they can assume both value
+        assert(net.valueMap[ID]().forall {
+          case (mid, r) if Set(2,4) contains mid => r == 0 || r == 7
+          case (mid, r) if Set(0,1,3) contains mid => r == 0
+          case (mid, r) if Set(5,6,7) contains mid => r == 7
+          case (8, 8) => true
+          case _ => false
+        })
+      }
+    }
   }
 
 }
