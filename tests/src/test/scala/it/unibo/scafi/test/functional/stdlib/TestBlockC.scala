e28d901a924f14bfd9ae7591ce767922f8875c6b
luca.tremamunno <luca.tremamunno@studio.unibo.it>
2020-11-06 18:18:17 +0100

    +Test reuse

--- tests/src/test/scala/it/unibo/scafi/test/functional/stdlib/TestBlockC.scala
+++ tests/src/test/scala/it/unibo/scafi/test/functional/stdlib/TestBlockC.scala
@@ -17,33 +17,52 @@ class TestBlockC extends FunSpec with BeforeAndAfterEach {
 
   val defaultNtimes: Int = someRounds
 
-  private[this] trait TestProgramStandard extends AggregateProgram with ScafiStandardLanguage with ScafiStandardLanguageLibraries
-    with StandardSensors with BlockC {
+  private[this] trait TestProgramDefs {
+    def hopGradient(source: Boolean): Double
+  }
 
-    def hopGradient(source: Boolean): Double =
+  private[this] trait TestProgramStandard extends AggregateProgram with ScafiStandardLanguage with ScafiStandardLanguageLibraries
+    with StandardSensors with BlockC with TestProgramDefs {
+
+    override def hopGradient(source: Boolean): Double =
       rep(Double.PositiveInfinity) { hops => {mux(source) {0.0} {1 + minHood(nbr {hops})}}}
   }
 
   private[this] trait TestProgramFC extends AggregateProgram with ScafiFCLanguage with ScafiFCLanguageLibraries
-    with StandardSensors with BlockC {
+    with StandardSensors with BlockC with TestProgramDefs {
 
-    def hopGradient(source: Boolean): Double =
+    override def hopGradient(source: Boolean): Double =
       rep(Double.PositiveInfinity) { hops => {mux(source) {0.0} {1 + nbrField{hops}.minHood}}}
   }
 
+  private[this] type ProgramImplDependencies = BlockC with ScafiBaseLanguage with TestProgramDefs
 
-  describe(s"BlockC for ScafiStandard") {
+
+  private val inStandard = "in ScafiStandardLanguage"
+  private val inFC = "in ScafiFCLanguage"
+  describe(s"BlockC") {
     describe("should support findParent") {
-      it("should work with a constant value") {
-        exec(new TestProgramStandard {
+      describe("should work with a constant value") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = findParent(1)
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue
-        )).toMap)(net)
+          assertNetworkValues((0 to 8).zip(List(
+            Int.MaxValue, Int.MaxValue, Int.MaxValue,
+            Int.MaxValue, Int.MaxValue, Int.MaxValue,
+            Int.MaxValue, Int.MaxValue, Int.MaxValue
+          )).toMap)(net)
+        }
+
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
       /*
         mid() induces a gradient field
@@ -51,152 +70,122 @@ class TestBlockC extends FunSpec with BeforeAndAfterEach {
           3  4  5
           6  7  8 )
       * */
-      it("should work with mid") {
-        exec(new TestProgramStandard {
+      describe("should work with mid") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = findParent(mid())
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, 0, 1,
-          0, 0, 1,
-          3, 3, Int.MaxValue
-        )).toMap)(net)
+          assertNetworkValues((0 to 8).zip(List(
+            Int.MaxValue, 0, 1,
+            0, 0, 1,
+            3, 3, Int.MaxValue
+          )).toMap)(net)
+        }
+
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
     }
     describe("should support C") {
       def sum: (Int, Int) => Int = _ + _
-      it("should produce a constant filed if every node has the same potential") {
-        exec(new TestProgramStandard {
+      describe("should produce a constant filed if every node has the same potential") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = C(1, sum, Int.MaxValue, 0)
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue
-        )).toMap)(net)
+          assertNetworkValues((0 to 8).zip(List(
+            Int.MaxValue, Int.MaxValue, Int.MaxValue,
+            Int.MaxValue, Int.MaxValue, Int.MaxValue,
+            Int.MaxValue, Int.MaxValue, Int.MaxValue
+          )).toMap)(net)
+        }
+
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
-      it("should accumulate on the device with the lowest potential") {
-        exec(new TestProgramStandard {
+      describe("should accumulate on the device with the lowest potential") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = C(mid(), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          8, 3, 1,
-          3, 1, 1,
-          1, 1, 1
-        )).toMap)(net)
+          assertNetworkValues((0 to 8).zip(List(
+            8, 3, 1,
+            3, 1, 1,
+            1, 1, 1
+          )).toMap)(net)
+        }
+
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
-      it("should accumulate along a gradient - one source") {
-        net.addSensor[Boolean]("source", false)
-        net.chgSensorValue("source", Set(4), true)
-        exec(new TestProgramStandard {
+      describe("should accumulate along a gradient - one source") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = C(hopGradient(sense("source")), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          net.addSensor[Boolean]("source", false)
+          net.chgSensorValue("source", Set(4), true)
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          1, 1, 1,
-          1, 8, 1,
-          1, 1, 1
-        )).toMap)(net)
+          assertNetworkValues((0 to 8).zip(List(
+            1, 1, 1,
+            1, 8, 1,
+            1, 1, 1
+          )).toMap)(net)
+        }
+
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
-      it("should accumulate along a gradient - two sources") {
-        net.addSensor[Boolean]("source", false)
-        net.chgSensorValue("source", Set(0,7), true)
-        exec(new TestProgramStandard {
+      describe("should accumulate along a gradient - two sources") {
+        trait ProgramMain extends AggregateProgram {
+          self: ProgramImplDependencies =>
           override def main(): ID = C(hopGradient(sense("source")), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
+        }
+        def doTest(program: AggregateProgram) {
+          net.addSensor[Boolean]("source", false)
+          net.chgSensorValue("source", Set(0,7), true)
+          exec(program, ntimes = defaultNtimes)(net)
 
-        assertNetworkValues((0 to 8).zip(List(
-          5, 2, 1,
-          1, 1, 1,
-          1, 3, 1
-        )).toMap)(net)
-      }
-    }
-  }
+          assertNetworkValues((0 to 8).zip(List(
+            5, 2, 1,
+            1, 1, 1,
+            1, 3, 1
+          )).toMap)(net)
+        }
 
-  //TODO now it is copy-paste, can do better?
-  describe(s"BlockC for ScafiFC") {
-    describe("should support findParent") {
-      it("should work with a constant value") {
-        exec(new TestProgramFC {
-          override def main(): ID = findParent(1)
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue
-        )).toMap)(net)
-      }
-      /*
-        mid() induces a gradient field
-        ( 0  1  2
-          3  4  5
-          6  7  8 )
-      * */
-      it("should work with mid") {
-        exec(new TestProgramFC {
-          override def main(): ID = findParent(mid())
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, 0, 1,
-          0, 0, 1,
-          3, 3, Int.MaxValue
-        )).toMap)(net)
-      }
-    }
-    describe("should support C") {
-      def sum: (Int, Int) => Int = _ + _
-      it("should produce a constant filed if every node has the same potential") {
-        exec(new TestProgramFC {
-          override def main(): ID = C(1, sum, Int.MaxValue, 0)
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue,
-          Int.MaxValue, Int.MaxValue, Int.MaxValue
-        )).toMap)(net)
-      }
-      it("should accumulate on the device with the lowest potential") {
-        exec(new TestProgramFC {
-          override def main(): ID = C(mid(), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          8, 3, 1,
-          3, 1, 1,
-          1, 1, 1
-        )).toMap)(net)
-      }
-      it("should accumulate along a gradient - one source") {
-        net.addSensor[Boolean]("source", false)
-        net.chgSensorValue("source", Set(4), true)
-        exec(new TestProgramFC {
-          override def main(): ID = C(hopGradient(sense("source")), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          1, 1, 1,
-          1, 8, 1,
-          1, 1, 1
-        )).toMap)(net)
-      }
-      it("should accumulate along a gradient - two sources") {
-        net.addSensor[Boolean]("source", false)
-        net.chgSensorValue("source", Set(0,7), true)
-        exec(new TestProgramFC {
-          override def main(): ID = C(hopGradient(sense("source")), sum, 1, 0)
-        }, ntimes = defaultNtimes)(net)
-
-        assertNetworkValues((0 to 8).zip(List(
-          5, 2, 1,
-          1, 1, 1,
-          1, 3, 1
-        )).toMap)(net)
+        it(inStandard) {
+          doTest(new TestProgramStandard with ProgramMain)
+        }
+        it(inFC) {
+          doTest(new TestProgramFC with ProgramMain)
+        }
       }
     }
   }
