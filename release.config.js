3e01f26b5bac9a01a7abdbe0cbd0f8f01da96021
Danilo Pianini <danilo.pianini@unibo.it>
2022-03-04 16:12:29 +0100

    ci(release): setup semantic-release

---
+++ release.config.js
@@ -0,0 +1,22 @@
+var workflowCmd = '::set-output name=has-released::'
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version} || exit 6
+sbt ci-release
+echo '${workflowCmd}true' 
+`
+console.log(`${workflowCmd}false`)
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.tagFormat('v${version}')
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            { "path": "build/shadow/*-all.jar" },
+        ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
